17305e1935b0f1b37baabd1426b0db9a32bc26b6
Prykhodko-Illia <iprykhodko@kse.org.ua>
2025-06-04 18:43:04 +0300

    Restoring files

---
+++ ceasar_encryption.cpp
@@ -0,0 +1,16 @@
+﻿#include "ceasar_encryption.h"
+#include <iostream>
+
+char *encrypt(char *rawText, int key) {
+    for (int i = 0; rawText[i] != '\0'; i++) {
+        rawText[i] = char(int(rawText[i]) + key);
+    }
+    return rawText;
+};
+
+char *decrypt(char *encryptedText, int key) {
+    for (int i = 0; encryptedText[i] != '\0'; i++) {
+        encryptedText[i] = char(int(encryptedText[i]) - key);
+    }
+    return encryptedText;
+}
