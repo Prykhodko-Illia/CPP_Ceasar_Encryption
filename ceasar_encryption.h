cc98d10711b450155cbefa2b8391326db7f7b7a1
Prykhodko-Illia <iprykhodko@kse.org.ua>
2025-06-04 19:27:26 +0300

    Executable file was created using static library

--- ceasar_encryption.h
+++ ceasar_encryption.h
@@ -1,9 +1,7 @@
 ﻿#ifndef CEASAR_ENCRYPTION_H
 #define CEASAR_ENCRYPTION_H
 
-extern "C" {
-    __declspec(dllexport) char *encrypt(char *rawText, int key);
+char *encrypt(char *rawText, int key);
+char *decrypt(char *encryptedText, int key);
 
-    __declspec(dllexport) char *decrypt(char *encryptedText, int key);
-}
 #endif //CEASAR_ENCRYPTION_H
--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
 ﻿#include <iostream>
-#include <windows.h>
+#include "ceasar_encryption.h"
 
 int main() {
-    typedef char *(*encrypt_t)(char *, int);
-    typedef char *(*decrypt_t)(char *, int);
-
-    HINSTANCE handle = LoadLibrary(TEXT("..//ceasar_encryption.dll"));
-    if (handle == nullptr || handle == INVALID_HANDLE_VALUE) {
-        std::cout << "Lib not found" << std::endl;
-        return 1;
-    }
-
-    encrypt_t encrypt = (encrypt_t)GetProcAddress(handle, TEXT("encrypt"));
-    if (encrypt == nullptr) {
-        std::cout << "The function is not found";
-        return 1;
-    }
-
-    decrypt_t decrypt = (decrypt_t)GetProcAddress(handle, TEXT("decrypt"));
-    if (decrypt == nullptr) {
-        std::cout << "The function is not found";
-        return 1;
-    }
-
     char *someText = new char[50];
     int key;
     std::cout << "Write text to encode: " << std::endl;
@@ -35,6 +14,5 @@ int main() {
     std::cout << decrypt(someText, key) << std::endl;
 
     delete[] someText;
-    FreeLibrary(handle);
     return 0;
 }
