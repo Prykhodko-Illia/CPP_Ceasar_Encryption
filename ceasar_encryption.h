8ec8415baf115f467d332579b1e7c76864916f18
Illia Prykhodko <iprykhodko@kse.org.ua>
2025-06-04 19:55:26 +0300

    Delete ceasar_encryption.h

--- ceasar_encryption.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef CEASAR_ENCRYPTION_H
-#define CEASAR_ENCRYPTION_H
-
-extern "C" {
-    __declspec(dllexport) char *encrypt(char *rawText, int key);
-
-    __declspec(dllexport) char *decrypt(char *encryptedText, int key);
-}
-#endif //CEASAR_ENCRYPTION_H
